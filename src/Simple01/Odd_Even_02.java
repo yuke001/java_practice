beec14282e3660e41b1d3e91d9e89a72362767b6
yuke001 <yukenthirans2018@gmail.com>
2025-03-19 11:10:13 +0530

    swap two number using temp

---
+++ src/Simple01/Odd_Even_02.java
@@ -0,0 +1,27 @@
+package Simple01;
+
+public class Odd_Even_02 {
+
+	public static void main(String args[]) {
+
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+		System.out.println();
+
+		System.out.println("***** 10 to 1 ***********");
+
+		for (int i = 10; i > 0; i--) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+	}
+
+}
---
+++ src/Simple01/Swap_two_Number_with_Temp_03.java
@@ -0,0 +1,23 @@
+package Simple01;
+
+public class Swap_two_Number_with_Temp_03 {
+	
+	public static void main(String args[]) {
+		
+		int a = 10 , b = 30 , temp = 0 ;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		temp = a ; 
+		a = b ; 
+		b = temp ; 
+		
+		
+		System.out.println();
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
